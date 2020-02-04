2f3150ee37f1b3d62114e8ca1346f34990dac53e
Kevin Liu <kevin07liu@gmail.com>
2020-02-04 15:08:42 +0800

    [8/38] Customize Admin

--- posts/admin.py
+++ posts/admin.py
@@ -6,4 +6,16 @@ from django.contrib import admin
 # admin.site.register([Post])
 from .models import Post
 
-admin.site.register([Post])
+
+class PostModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+
+    list_display = ["title", "updated", "timestamp"]
+    # list_display_links = ["updated"]
+    # list_editable = ["title"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ("title", "context")
+
+
+admin.site.register(Post, PostModelAdmin)
