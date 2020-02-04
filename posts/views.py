aff4f945d912e1c4cb5de52a3c3502e492125ce7
Kevin Liu <kevin07liu@gmail.com>
2020-02-04 19:22:25 +0800

    [11/38] Request & Response

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
+# the logic handle the request the browser/client make
 def post_home(request):
     return HttpResponse("<h1>Hello</h1>")
