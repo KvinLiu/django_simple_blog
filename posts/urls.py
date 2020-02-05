71bfc55f85be650882c06fd6175e70425af63dff
Kevin Liu <kevin07liu@gmail.com>
2020-02-05 17:19:08 +0800

    [19/38] URL Links & Get Absolute URL

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 
+app_name = "posts"
 urlpatterns = [
     path("", views.post_list, name="list"),
     path("create/", views.post_create, name="home"),
