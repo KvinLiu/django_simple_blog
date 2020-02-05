d80ab3a8267c8fb21b46572d98ba485dc635821d
Kevin Liu <kevin07liu@gmail.com>
2020-02-05 23:26:51 +0800

    [21/38] Instance Update View

--- posts/urls.py
+++ posts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("", views.post_list, name="list"),
     path("create/", views.post_create, name="home"),
     path("<int:id>", views.post_detail, name="detail"),
-    path("update/", views.post_update, name="update"),
+    path("<int:id>/edit/", views.post_update, name="update"),
     path("delete/", views.post_delete, name="delete"),
 ]
